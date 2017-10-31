1b5529ba03b0e62e0d34698221724689c56acf67
jakhani <januakhani.ce@gmail.com>
2017-10-31 14:09:22 -0700

    Making changes to integrate Drelephant with mysql of Kubernetes cluster (#4)
    * Making changes to integrate with mysql of Kubernetes cluster
    
    * Making changes to integrate with mysql of Kubernetes cluster

--- data-platform/50drelephant/frontend/entrypoint.sh
+++ data-platform/50drelephant/frontend/entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-db_url=dr-elephant-mysql
+db_url=mysql.data-platform
 db_name=drelephant
 db_user=root
-db_password="drelephant"
+db_password=password
 
-until mysql -h "$db_url" -u$db_user -p$db_password -D$db_name -e'show tables;'; do
-  >&2 echo "MySQL is unavailable - sleeping"
+until mysql -h "$db_url" -u$db_user -p$db_password -e 'create database $db_name;'; do
+  >&2 echo "MySQL is unavailable - sleeping. Unable to create database."
   sleep 1
 done
 
+until mysql -h "$db_url" -u$db_user -p$db_password -D$db_name -e'show tables;'; do
+  >&2 echo "MySQL is unavailable - sleeping. Unable to connect with database."
+  sleep 1
+done
 
-#db.default.url="jdbc:mysql://localhost/playdb"
-#db.default.username=playdbuser
-#db.default.password="a strong password"
-
-
-
+until mysql -h "$db_url" -u$db_user -p$db_password -D$db_name < /usr/dr-elephant-data/setup.sql; do
+  >&2 echo "MySQL is unavailable - sleeping. Unable to create tables."
+  sleep 1
 exec "$@"
