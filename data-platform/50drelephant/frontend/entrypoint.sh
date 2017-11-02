e7ee581b0c3584f41058cd41af98b218b1f0711a
jakhani <januakhani.ce@gmail.com>
2017-11-02 12:57:01 -0700

    Dr elephant memory reduced, mysql related fix, hdfs integration. (#6)
    * Making changes to integrate with mysql of Kubernetes cluster
    
    * Making changes to integrate with mysql of Kubernetes cluster
    
    * Fixing memory and hadoop configs

--- data-platform/50drelephant/frontend/entrypoint.sh
+++ data-platform/50drelephant/frontend/entrypoint.sh
@@ -5,17 +5,10 @@ db_name=drelephant
 db_user=root
 db_password=password
 
-until mysql -h "$db_url" -u$db_user -p$db_password -e 'create database $db_name;'; do
-  >&2 echo "MySQL is unavailable - sleeping. Unable to create database."
-  sleep 1
-done
+mysql -h "$db_url" -u$db_user -p$db_password -e 'CREATE DATABASE IF NOT EXISTS drelephant;'
 
-until mysql -h "$db_url" -u$db_user -p$db_password -D$db_name -e'show tables;'; do
-  >&2 echo "MySQL is unavailable - sleeping. Unable to connect with database."
-  sleep 1
-done
+mysql -h "$db_url" -u$db_user -p$db_password -D$db_name < /usr/dr-elephant/setup.sql
+sleep 100
 
-until mysql -h "$db_url" -u$db_user -p$db_password -D$db_name < /usr/dr-elephant-data/setup.sql; do
-  >&2 echo "MySQL is unavailable - sleeping. Unable to create tables."
-  sleep 1
+mysql -h "$db_url" -u$db_user -p$db_password -D$db_name -e'show tables;'
 exec "$@"
